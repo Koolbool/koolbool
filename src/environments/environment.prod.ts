80541f89fde7c11e69a5e068629ab08824b2e8f7
Lucas Fernandes Roncolato <fernal13@lsbu.ac.uk>
2022-12-17 15:50:21 +0000

    firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDcGusRDXYPV8qMMgjlOvDL3KmaBzt4Ias",
+    authDomain: "koolbool-1c1ea.firebaseapp.com",
+    projectId: "koolbool-1c1ea",
+    storageBucket: "koolbool-1c1ea.appspot.com",
+    messagingSenderId: "45768181416",
+    appId: "1:45768181416:web:985c5acef2493eebc0a49d"
+  }
 };
